1dd43e55a4a5d2e75a14eca0d196b7223aca3b12
Ross <rdaly525@stanford.edu>
2019-03-14 17:00:36 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='metamapper',
+    version='0.0.1',
+    url='https://github.com/rdaly525/MetaMapper',
+    license='MIT',
+    maintainer='Ross Daly',
+    maintainer_email='rdaly525@stanford.edu',
+    description='A Mapper constructor for CoreIR',
+    packages=[
+        "metamapper",
+    ],
+    install_requires=[],
+    python_requires='>=3.6'
+)
