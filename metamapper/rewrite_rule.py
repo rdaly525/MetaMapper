2254693fb22cf85e9666e60470204ddffce71288
Ross <rdaly525@stanford.edu>
2019-03-30 08:11:56 -0700

    started rewrite rule

--- metamapper/rewrite_rule.py
+++ metamapper/rewrite_rule.py
@@ -45,6 +45,43 @@ class Peak1to1(RewriteRule):
 
         return mapped_instances
 
+class Peak1to1Lambda(RewriteRule):
+    def __init__(self,coreir_prim : coreir.module.Module, peak_prim : coreir.module.Module, prim_instr_lambda : peak.ISABuilder, io_mapping):
+        print(io_mapping)
+        self.coreir_prim = coreir_prim
+        self.prim_instr_lambda = prim_instr_lambda
+        #Actually construct the coreir definition
+        coreir_def = coreir_prim.new_definition()
+        c = coreir_prim.context
+        param_name = prim_instr.__class__.__name__
+        modvalues = c.new_values({param_name : str(prim_instr)})
+
+        peak_inst = coreir_def.add_module_instance(name="inst",module=peak_prim,config=modvalues)
+        for coreir_port,peak_port in io_mapping.items():
+            print(coreir_port,peak_port)
+            pio = peak_inst.select(peak_port)
+            if coreir_port == "0":
+                coreir.connect_const(pio,0)
+            else:
+                cio = coreir_def.interface.select(coreir_port)
+                coreir_def.connect(pio,cio)
+        self.coredef = coreir_def
+
+    #returns a map from instance name to peak instr
+    def __call__(self,app : coreir.module.Module):
+        c = app.context
+        mdef = app.definition
+        assert mdef
+        mapped_instances = {}
+        for inst in mdef.instances:
+            inst_mod = inst.module
+            if inst_mod == self.coreir_prim:
+                mapped_instances[inst.name+"$inst"] = self.prim_instr
+                inst_mod.definition = self.coredef
+                coreir.inline_instance(inst)
+
+        return mapped_instances
+
 class PeakIO(RewriteRule):
     #Interpreting is_input as an input to the fabric which indicates the io_port_name is an output
     def __init__(self, width, is_input, io_prim : coreir.module.Module):
