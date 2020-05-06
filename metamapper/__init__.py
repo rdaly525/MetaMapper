4aa183b0bf002d59e2f2b6591321756b4414d237
Ross <rdaly525@stanford.edu>
2020-05-06 14:52:02 -0700

    Fixed issue with reseting coreir in magma

--- metamapper/__init__.py
+++ metamapper/__init__.py
@@ -2,8 +2,7 @@ import magma
 import coreir
 
 def CoreIRContext(reset=False) -> coreir.Context:
-    coreir_singleton = magma.backend.coreir_.CoreIRContextSingleton()
     if reset:
-        coreir_singleton.reset_instance()
-    return coreir_singleton.get_instance()
+        magma.frontend.coreir_.ResetCoreIR()
+    return magma.backend.coreir_.CoreIRContextSingleton().get_instance()
 
