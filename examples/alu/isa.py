84ed90faf2116058031b43b624b8f07544c748f8
Ross <rdaly525@stanford.edu>
2020-06-04 13:25:36 -0700

    more updates

--- examples/alu/isa.py
+++ examples/alu/isa.py
@@ -7,6 +7,13 @@ class OP(Enum):
     Or =  4
     And = 5
     XOr = 6
+    imm = 6
 
-class Inst(Product):
-    op = OP
+def gen_Inst(width):
+    @family_closure
+    def Inst_fc(family):
+        class Inst(Product):
+            op = OP
+            imm = family.BitVector[width]
+        return Inst
+    return Inst_fc
--- examples/alu/sim.py
+++ examples/alu/sim.py
@@ -1,15 +1,16 @@
-from peak import Peak, family_closure
-from .isa import Inst, OP
+from peak import Peak, family_closure, Const
+from .isa import gen_Inst, OP
 
 def gen_ALU(width):
+    Inst_fc = gen_Inst(width)
     @family_closure
     def ALU_fc(family):
         Data = family.BitVector[width]
         SData = family.Signed[width]
-
+        Inst = Inst_fc(family)
         @family.assemble(locals(), globals())
         class ALU(Peak):
-            def __call__(self, inst: Inst, a: Data, b: Data) -> Data:
+            def __call__(self, inst: Const(Inst), a: Data, b: Data) -> Data:
                 a = SData(a)
                 b = SData(b)
                 op = inst.op
@@ -21,8 +22,10 @@ def gen_ALU(width):
                     res = a & b
                 elif op == OP.Or:
                     res = a | b
-                else: # op == ALUOP.XOr:
+                elif op == OP.XOr:
                     res = a ^ b
+                else: # op == Op.imm
+                    res = inst.imm
                 return res
         return ALU
     return ALU_fc
