e45d61cc679fa45d9345ee8a57a84811f0d13490
Ross <rdaly525@stanford.edu>
2020-08-02 09:00:24 -0700

    verilog generation should be working

--- tests/test_full.py
+++ tests/test_full.py
@@ -8,7 +8,10 @@ import metamapper.peak_util as putil
 from metamapper.node import Nodes
 from metamapper import CoreIRContext
 from metamapper.coreir_mapper import Mapper
+
+import delegator
 import pytest
+
 
 lassen_constraints = {
     ("clk_en",): 1,
@@ -18,11 +21,11 @@ lassen_constraints = {
 }
 
 @pytest.mark.parametrize("arch", [
-    ("PE_lut", gen_PE_lut(16), {}),
+    #("PE_lut", gen_PE_lut(16), {}),
     #("Lassen", lassen_fc, lassen_constraints),
-    ("ALU", gen_ALU(16), {}),
+    #("ALU", gen_ALU(16), {}),
 ])
-#@pytest.mark.parametrize("app", ["camera_pipeline"])#, "add2", "add1_const", "add4", "add3_const"])
+#@pytest.mark.parametrize("app", ["camera_pipeine"])#, "add2", "add1_const", "add4", "add3_const"])
 #@pytest.mark.parametrize("app", ["conv_3_3"])#, "add2", "add1_const", "add4", "add3_const"])
 @pytest.mark.parametrize("app", ["add2", "add1_const", "add4", "add3_const"])
 def test_app(arch, app):
@@ -30,7 +33,7 @@ def test_app(arch, app):
     file_name = f"examples/coreir/{app}.json"
     cutil.load_libs(["commonlib"])
     CoreIRNodes = gen_CoreIRNodes(16)
-    cmod = cutil.load_from_json(file_name) # libraries=["lakelib"])
+    cmod = cutil.load_from_json(file_name, libraries=["lakelib"])
     pb_dags = cutil.preprocess(CoreIRNodes, cmod)
     name, arch_fc, constraints = arch
     if name == "ALU" and app == "add_or":
@@ -38,7 +41,20 @@ def test_app(arch, app):
     ArchNodes = Nodes("Arch")
     putil.load_from_peak(ArchNodes, arch_fc)
     mapper = Mapper(CoreIRNodes, ArchNodes, conv=True)
-    mod = mapper.do_mapping(pb_dags)
-    mod.print_()
-    #c.run_passes(["cullgraph"])
-    mod.save_to_file(f"tests/build/{name}_{app}_mapped.json")
+    mapped_cmod = mapper.do_mapping(pb_dags)
+    mapped_cmod.print_()
+    c.set_top(mapped_cmod)
+    c.run_passes(["cullgraph"])
+    mapped_file = f"tests/build/{name}_{app}_mapped"
+    mapped_cmod.save_to_file(f"{mapped_file}.json")
+
+    #Test syntax of serialized json
+    res = delegator.run(f"coreir -i {mapped_file}.json -l commonlib")
+    assert not res.return_code, res.out + res.err
+
+    #Test serializing to verilog
+    res = delegator.run(f'coreir -i {mapped_file}.json -l commonlib -p "wireclocks-clk; wireclocks-arst" -o {mapped_file}.v --inline')
+    assert not res.return_code, res.out + res.err
+
+#test_app(("PE_lut", gen_PE_lut(16), {}),"add2")
+
