20ffc83d3834f79cfd1bade0b059fef3ced303c5
Ross <rdaly525@stanford.edu>
2021-04-08 21:01:58 -0700

    removes img gen

--- tests/test_basic_mapping.py
+++ tests/test_basic_mapping.py
@@ -58,7 +58,8 @@ def test_kernel_mapping(app):
 
     mapper = Mapper(IRNodes, ArchNodes, lazy=True, rule_file=lassen_rules)
     mapped_dag = mapper.do_mapping(dag, convert_unbound=False, prove_mapping=False)
-    gen_dag_img(mapped_dag, f"img/{app}_mapped")
+
+    #gen_dag_img(mapped_dag, f"img/{app}_mapped")
 
     mod = cutil.dag_to_coreir(ArchNodes, mapped_dag, f"{app_name}_mapped", convert_unbounds=False)
     c.serialize_definitions(build_file, [mod])
@@ -90,24 +91,3 @@ def test_post_mapped_loading(app):
         {"global.MEM": MEM_fc},
     )
     app_name = cmod.name
-    dag = cutil.coreir_to_dag(IRNodes, cmod)
-    gen_dag_img(dag, f"img/{app}_mapped")
-    print_dag(dag)
-
-
-    #arch_fc = lassen_fc
-    #ArchNodes = Nodes("Arch")
-    #putil.load_and_link_peak(ArchNodes, lassen_header, {"global.PE": arch_fc})
-
-    ##rule_file = lassen_rules
-    ##mapper = Mapper(IRNodes, ArchNodes, lazy=True, rule_file=rule_file)
-    ##mapped_dag = mapper.do_mapping(dag, convert_unbound=False, prove_mapping=False)
-
-    #mod = cutil.dag_to_coreir(ArchNodes, dag, f"{app_name}_mapped_mapped", convert_unbounds=False)
-    #mod.print_()
-    #output_file = f"tests/build/{app}_mapped_mapped.json"
-    #c.serialize_definitions(output_file, [mod])
-
-
-
-
