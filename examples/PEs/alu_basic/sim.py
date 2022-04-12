c90631aa9ba7781bbeb02aa01c27b3425ea53c5d
ross <rdaly525@stanford.edu>
2022-04-11 20:53:03 -0700

    Fixes rewrite test

--- examples/PEs/alu_basic/sim.py
+++ examples/PEs/alu_basic/sim.py
@@ -5,13 +5,15 @@ def gen_ALU(width):
     isa_fc = gen_isa(width)
     @family_closure
     def ALU_fc(family):
-        isa = isa_fc(family)
+        isa_fam = isa_fc(family)
+        isa = isa_fc.Py
+
         @family.assemble(locals(), globals())
         class ALU(Peak):
             @name_outputs(out=isa.Data)
             def __call__(self, inst: Const(isa.Inst), a: isa.Data, b: isa.Data) -> isa.Data:
-                a = isa.SData(a)
-                b = isa.SData(b)
+                a = isa_fam.SData(a)
+                b = isa_fam.SData(b)
                 op = inst.op
                 if op == isa.OP.imm:
                     res = inst.imm
